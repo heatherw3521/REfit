eecf4d16fafdbdaaba6fb5efb2b88d5911271d94
heatherw3521 <heatherw3521@gmail.com>
2026-06-18 10:13:38 -0700

    make residues for efun work in z transform sense

--- @efun/residues.m
+++ @efun/residues.m
@@ -1,17 +1,22 @@
-function out = residues(s, varargin)
-% residues(s, 'zt') returns the residues of the poles of r(z), 
+function [res, const] = residues(s, varargin)
+% residues(s) returns the residues of the poles of r(z), 
 % where r = ift(s), r(z) = r(x) on the unit circle,
 % and z = exp(2*pi*1i*(x-a)/(b-a)) with x defined on [a, b].
 
-w = s.weights;
-if isempty(s)
-    out = []; 
-end
 
-if isempty(varargin)
-    out = 'to do';
-else
-    out = w.*exp(s.exp); 
-end
+ if isempty(s)
+    res = [];
+    const = []; 
+    return
+ end
+
+ if strcmpi(varargin{1}, 'zt')
+    w = s.weights; 
+    const = s.const; 
+    scl = s.scl; 
+    res = scl*[w.*exp(s.exp); -conj(w).*exp(-conj(s.exp))];
+ else
+    error('for now residues are only available with zt flag')
+ end
 
 end
